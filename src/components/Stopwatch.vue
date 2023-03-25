b0973918cae0bb71c01cd22976ab703bc9d21551
Paronim <Zreba.v@gmial.com>
2023-03-25 23:51:51 +0300

    Создание списка секундомеров

--- src/components/Stopwatch.vue
+++ src/components/Stopwatch.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <div class="wrapper-stopwatch">
+    <div class="stopwatch-list" v-for="( stopwatch, index ) in stopwatches" :key="index">
+      <div class="stopwatch-element">
+        {{ stopwatch }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue'
 
+  const stopwatches = ref([0])
 </script>
 
 <style lang="scss">
